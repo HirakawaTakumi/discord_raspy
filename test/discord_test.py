4bee8af62d81b328514d8432cc412688aa902ded
TakumiHirakawa <hirakawa770@gmail.com>
2021-06-17 22:57:19 +0900

    add test.py

---
+++ test/discord_test.py
@@ -0,0 +1,46 @@
+import discord
+from discord import message
+from discord import channel
+import sys
+
+with open('token.txt', 'r') as f:
+    data = f.readline()
+TOKEN = data
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+    channel = client.get_channel(846438705098784808)
+    print(channel)
+    await channel.send('ログインしたよ！')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+    
+    if message.content == '/hello':
+        await message.channel.send('ハロー！')
+
+    # quitでbotの停止（id指定ではなくロールで判断したい）
+    if message.content == '/quit':
+        if message.author.id == 395507828162560000:
+            await message.channel.send('バイバイ！また遊んでね')
+            await client.logout()
+            await sys.exit()
+        else:
+            await message.channel.send('君は僕を止めることができない。。。')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
